c32a53d171594551a7c860f72f8069e64894f620
raushanr7n <149138499+raushanr7n@users.noreply.github.com>
2024-03-29 18:09:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use Strict"; // treat all JS code as newer version
+
+// alert(3 + 3)   //we are using nodejs, not browser
+
+console.log(3 
+    +3) // code readbility should be high
+
+console.log("Raushan");
+
+let name = "Raushan"
+let age = 18
+let isLoggedIn = false
+let number = null;
+
+
+// nimber => 2 to power 53
+// bigint => 
+//string => ""
+// boolean => true/false
+// null => standlone value
+// undefidend => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof  undefined); //undefined
+console.log(typeof null); // object
