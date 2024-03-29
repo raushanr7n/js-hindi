8204f2ce5f3bd41c23900f6833a583d10916c617
raushanr7n <149138499+raushanr7n@users.noreply.github.com>
2024-03-29 18:44:44 +0000

    Conversion Of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = "33adf"
+
+// let score = null
+
+// let score = undefined
+
+
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+
+
+//  "33" => 33
+//  "333fsd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Raushan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "raushan" => true
+
+
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
