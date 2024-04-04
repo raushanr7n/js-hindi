d32722b5a7cff1a8716122305c5a564cd749e000
Raushan Kumar <149138499+raushanr7n@users.noreply.github.com>
2024-04-04 06:19:10 +0000

    num and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+
+// console.log(typeof score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString(). length.valueOf);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 124.8869
+console.log(otherNumber);
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ************************* Maths *************************
+
+// console.log(Math);
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+
+
+// console.log(Math.min(8,3,2,8,));
+
+console.log(Math.random());
+console.log((Math.random()* 10) +1);
+console.log(Math.floor(Math.random()* 10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
