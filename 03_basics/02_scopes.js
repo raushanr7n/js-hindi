9bf3fdb879c7a4a4a05037ecbd477f10ffbac59d
Raushan Kumar <149138499+raushanr7n@users.noreply.github.com>
2024-04-14 16:36:03 +0000

    Nested scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,8 +5,51 @@ let a = 300
 if (true) {
     let a = 10
     var b = 201
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
+
+
+// Nested scope
+
+function one(){
+    const username = "raushan"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        // console.log(website);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+
+if (true) {
+    const username = "raushan"
+    if(username === "raushan"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// ***************** intersting *****************
+
+function addone(num,){
+    return num + 1
+}
+console.log(addone(5))
+
+addtwo(5)
+const addtwo = function(num){
+    return num + 2
+}
